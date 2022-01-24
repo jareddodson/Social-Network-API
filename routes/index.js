14f7c21b9b5c53316f9d2c212ee2e486735ba749
jareddodson <jared.dodson04@gmail.com>
2022-01-23 20:22:30 -0600

    fixed index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 router.use((req, res) => {
     res.status(404).send('Error 404')
